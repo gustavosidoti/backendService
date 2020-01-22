8f05b8dfe866f4fb8ce324288af6df5ad559cdfd
gustavosidoti <gustavosidoti@gmail.com>
2020-01-21 23:39:51 -0300

    sección 12 terminada backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+
+// Google
+module.exports.CLIENT_ID = '970187823155-hrv1ghlo83qfuc0lb2rbumjghtf1ht3h.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -8,9 +8,119 @@ var app = express(); // levantamos el express
 
 var Usuario = require('../models/usuario'); // me permite utilizar las funciones que el modelo de usuario tiene definido
 
+// Google
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
 
 // =====================================================
-//           LOGIN DE USUARIO USUARIO
+//           LOGIN DE USUARIO USUARIO Autenticación de Google
+// =====================================================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+
+    }
+}
+
+app.post('/google', async(req, res) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch(e => {
+            res.status(403).json({
+                ok: false,
+                mensaje: 'token no valido'
+
+            });
+        });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuarios',
+                errors: err
+            });
+        }
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) { // si el usuario no fue autenticado por google
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe usar su autenticación normal',
+
+                });
+            } else { // si el usuario está autenticado debo refrescar el token
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // payload: la data que queramos que vaya. luego duración 4 horas
+
+                // respuesta del servidor si todo salió bien
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+
+                });
+            }
+        } else {
+            // El usuario no existe hay que crearlo
+            var usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+
+            usuario.save((err, usuarioDB) => {
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // payload: la data que queramos que vaya. luego duración 4 horas
+
+                // respuesta del servidor si todo salió bien
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+
+                });
+
+            });
+        }
+    });
+
+
+    // return res.status(200).json({
+    //     ok: true,
+    //      mensaje: 'OK.!!',
+    //      googleUser: googleUser
+
+    // });
+
+
+});
+
+
+
+
+// =====================================================
+//           LOGIN DE USUARIO USUARIO Autenticación Normal
 // =====================================================
 
 app.post('/', (req, res) => {
